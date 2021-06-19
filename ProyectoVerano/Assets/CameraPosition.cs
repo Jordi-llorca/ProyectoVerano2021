4e86083a40b83763955be07d9f9f9a0c693ea438
Jordi <jorllorber@gmail.com>
2021-06-19 13:21:46 +0200

    Revert "Revert "Primeras Cosas""
    This reverts commit eaa855f0816ce5957e58d9dd4b5646e6f3a7d9b9.

---
+++ ProyectoVerano/Assets/CameraPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+
+    public Camera cam;
+    public Transform [] positions;
+    private int actualPos = 0;
+
+    public int getPositionCamera() { return actualPos; }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)){
+            actualPos = ((actualPos - 1) + 4) % 4;
+            cam.transform.SetPositionAndRotation(positions[actualPos].position, positions[actualPos].rotation);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            actualPos = ((actualPos + 1) + 4) % 4;
+            cam.transform.SetPositionAndRotation(positions[actualPos].position, positions[actualPos].rotation);
+        }
+    }
+}
