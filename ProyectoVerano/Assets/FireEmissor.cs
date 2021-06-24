b5a6444d37d354d7b97c39dbb64c4aa2f438a073
Jordi <jorllorber@gmail.com>
2021-06-24 11:00:38 +0200

    Muchas cosas jeje

---
+++ ProyectoVerano/Assets/FireEmissor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEmissor : MonoBehaviour
+{
+    public float interval = 2f;
+    private float time;
+    private ParticleSystem ps;
+
+    private bool isOn;
+
+    void Start()
+    {
+        ps = this.GetComponent<ParticleSystem>();
+        time = 0;
+    }
+
+    void Update()
+    {
+        if (time > 0) time -= Time.deltaTime;
+        else
+        {
+            time = interval;
+            if (isOn) ps.Play();
+            else ps.Stop();
+            isOn = !isOn;
+        }
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerMovement>().Morir();
+        }
+    }
+}
--- ProyectoVerano/Assets/PlayerMovement.cs
+++ ProyectoVerano/Assets/PlayerMovement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float vel = 1f;
+    public float movementSpeed = 15;
+    public float rotationSpeed = 4;
     Rigidbody rb;
 
     CameraPosition camPos;
+    public GameObject cam;
+
+    Vector3 dir;
+
+    bool canMove = true;
 
     void Start()
     {
@@ -15,27 +21,52 @@ public class PlayerMovement : MonoBehaviour
         camPos = FindObjectOfType<CameraPosition>();
     }
 
-
     void Update()
+    {
+        if (canMove) movement();
+    }
+
+    void movement()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        dir = new Vector3(-v, 0, h);
 
         switch (camPos.getPositionCamera())
         {
             case 0:
-                rb.velocity = new Vector3(h * vel, v != 0 || h != 0 ? 0 : -5 , v * vel);
+                dir = new Vector3(-v, 0, h);
                 break;
             case 1:
-                rb.velocity = new Vector3(-v * vel, v != 0 || h != 0 ? 0 : -5, h * vel);
+                dir = new Vector3(-h, 0, -v);
                 break;
             case 2:
-                rb.velocity = new Vector3(-h * vel, v != 0 || h != 0 ? 0 : -5, -v * vel);
+                dir = new Vector3(v, 0, -h);
                 break;
             case 3:
-                rb.velocity = new Vector3(v * vel, v != 0 || h != 0 ? 0 : -5, -h * vel);
+                dir = new Vector3(h, 0, v);
                 break;
         }
-        
+
+
+        if (v != 0 || h != 0) rb.MovePosition(rb.position + transform.right * Mathf.Max(Mathf.Abs(h), Mathf.Abs(v)) * movementSpeed * Time.deltaTime);
+
+        if (dir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                Quaternion.LookRotation(dir),
+                Time.deltaTime * rotationSpeed
+            );
+        }
+    }
+
+    public void blockMovement(bool move){ canMove = !move; }
+
+    public Vector3 getDirection() { return dir; }
+
+    public void Morir()
+    {
+        Debug.Log("Muerto :(");
     }
 }
